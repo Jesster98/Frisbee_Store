8316b50c6635c3a64af135a2ff9496521828779c
Jesse Johnson <jessejohnson100@yahoo.com>
2017-02-15 21:00:30 +0000

    second try

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,48 +1,78 @@
-require 'test_helper'
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-class ProductsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @product = products(:one)
+  # GET /products
+  # GET /products.json
+  def index
+    @products = Product.all
   end
 
-  test "should get index" do
-    get products_url
-    assert_response :success
+  def admin
+    @products = Product.all
   end
 
-  test "should get new" do
-    get new_product_url
-    assert_response :success
+  # GET /products/1
+  # GET /products/1.json
+  def show
   end
 
-  test "should create product" do
-    assert_difference('Product.count') do
-      post products_url, params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products
+  # POST /products.json
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
     end
 
-    assert_redirected_to product_url(Product.last)
-  end
-
-  test "should show product" do
-    get product_url(@product)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_product_url(@product)
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch product_url(@product), params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
-    assert_redirected_to product_url(@product)
-  end
-
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:title, :description, :image_url, :price)
     end
-
-    assert_redirected_to products_url
-  end
 end
